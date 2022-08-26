fc0ad7d2bc3b7b4041d610c21f48d7c98a7c8169
Melina Figueroa <melinafigueroa@tdf.edu.ar>
2022-08-25 21:15:11 -0300

    tp fina, comence con .js

--- html/script/script.js
+++ html/script/script.js
@@ -1,6 +1,14 @@
-let btnSiguiente = document.getElementById("btnSiguiente");
-btnSiguiente.addEventListener("click", cambiarPagina);
+"use strict"
+let producto = document.getElementById("productosEnStock");
+let stock = [1023,1000,560,487,300,150];
+let precioUnidad = [670,209,175,324,1750,535];
+let total = 0;
+let btnComprar = document.getElementById("botonComprar");
+btnComprar.addEventListener("click", comprarProducto)
 
-function cambiarPagina(){
-    
+function comprarProducto(){
+ for (let i = 0; i < stock.length; i++) {
+ total= parseInt(producto[i]) * parseInt(precioUnidad[i]);
+     console.log(total);
+ } 
 }
